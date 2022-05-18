a6e55934ce052463b7c371fb66721382593cc969
ZRShamim <zrshamim8822@gmail.com>
2022-05-18 12:31:11 +0600

    Detail page update

--- lib/view/page/movie_detail_page.dart
+++ lib/view/page/movie_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../root_page.dart';
 
@@ -10,7 +11,7 @@ class MovieDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // extendBody: true,
-      bottomNavigationBar: BottomNavBar(),
+      bottomNavigationBar: const BottomNavBar(),
       body: Container(
         height: MediaQuery.of(context).size.height,
         decoration: const BoxDecoration(
@@ -21,15 +22,128 @@ class MovieDetailsPage extends StatelessWidget {
             ],
           ),
         ),
+        child: Stack(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.6,
+              child: Image.network(
+                'https://comic-cons.xyz/wp-content/uploads/thor-marvel-movies.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.5,
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                height: MediaQuery.of(context).size.height * 0.5,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xff2B5876),
+                      Color(0xff4E4376),
+                    ],
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Thor',
+                      style: GoogleFonts.muli(
+                        fontSize: 64,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'The Dark World',
+                      style: GoogleFonts.muli(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Chip(
+                              backgroundColor: Colors.blueGrey[300],
+                              label: Text(
+                                'Action',
+                                style: GoogleFonts.muli(
+                                  fontSize: 12,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            Chip(
+                              backgroundColor: Colors.blueGrey[300],
+                              label: Text(
+                                '16+',
+                                style: GoogleFonts.muli(
+                                  fontSize: 12,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            Chip(
+                              backgroundColor: Colors.amber,
+                              label: Text(
+                                'IMDB 8.5',
+                                style: GoogleFonts.muli(
+                                  fontSize: 12,
+                                  textStyle: const TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.share,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.favorite,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
 class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({
-    Key? key,
-  }) : super(key: key);
+  const BottomNavBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -61,75 +175,43 @@ class BottomNavBar extends StatelessWidget {
           color: Colors.transparent,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(
-                onPressed: () {
-                  Get.offAll(
-                    () => RootPage(
-                      i: 0,
-                    ),
-                  );
-                },
-                icon: const Icon(
-                  Icons.home_outlined,
-                  color: Colors.white,
-                ),
-              ),
-              IconButton(
-                onPressed: () {
-                  Get.offAll(
-                    () => RootPage(
-                      i: 1,
-                    ),
-                  );
-                },
-                icon: const Icon(
-                  Icons.favorite_outlined,
-                  color: Colors.white,
-                ),
-              ),
-              IconButton(
-                onPressed: () {
-                  Get.offAll(
-                    () => RootPage(
-                      i: 2,
-                    ),
-                  );
-                },
-                icon: const Icon(
-                  Icons.confirmation_number_outlined,
-                  color: Colors.white,
-                ),
-              ),
-              IconButton(
-                onPressed: () {
-                  Get.offAll(
-                    () => RootPage(
-                      i: 3,
-                    ),
-                  );
-                },
-                icon: const Icon(
-                  Icons.person_outline_outlined,
-                  color: Colors.white,
-                ),
-              ),
-              IconButton(
-                onPressed: () {
-                  Get.offAll(
-                    () => RootPage(
-                      i: 4,
-                    ),
-                  );
-                },
-                icon: const Icon(
-                  Icons.share_outlined,
-                  color: Colors.white,
-                ),
-              ),
+            children: const [
+              BottomNavBarItem(0, Icons.home_outlined),
+              BottomNavBarItem(1, Icons.favorite_outlined),
+              BottomNavBarItem(2, Icons.confirmation_number_outlined),
+              BottomNavBarItem(3, Icons.person_outline_outlined),
+              BottomNavBarItem(4, Icons.share_outlined),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class BottomNavBarItem extends StatelessWidget {
+  const BottomNavBarItem(
+    this.index,
+    this.icon, {
+    Key? key,
+  }) : super(key: key);
+
+  final int index;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        Get.offAll(
+          () => RootPage(
+            i: index,
+          ),
+        );
+      },
+      icon: Icon(
+        icon,
+        color: Colors.white,
       ),
     );
   }
